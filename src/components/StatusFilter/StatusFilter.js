b381b21954325eea96e81f7be122368dbf6214aa
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-10 19:12:37 +0100

    init

---
+++ src/components/StatusFilter/StatusFilter.js
@@ -0,0 +1,16 @@
+import { Button } from "../../components/Button/Button";
+import css from "./StatusFilter.module.css";
+import { useSelector } from "react-redux";
+import { statusFilters } from "../../redux/constants";
+
+export const StatusFilter = () => {
+	const filter = useSelector((state) => state.filters.status);
+
+	return (
+		<div className={css.wrapper}>
+			<Button selected={filter === statusFilters.all}>All</Button>
+			<Button selected={filter === statusFilters.active}>Active</Button>
+			<Button selected={filter === statusFilters.completed}>Completed</Button>
+		</div>
+	);
+};
