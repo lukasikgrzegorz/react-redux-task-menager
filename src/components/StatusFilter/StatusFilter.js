473d3513e94feb368ad02468149844e73c1df340
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-10 22:28:35 +0100

    create actions

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,17 +1,34 @@
 import { Button } from "../../components/Button/Button";
 import css from "./StatusFilter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { statusFilters } from "../../redux/constants";
-
+import { setStatusFilter } from "../../redux/actions";
 
 export const StatusFilter = () => {
+	const dispatch = useDispatch();
 	const filter = useSelector((state) => state.filters.status);
+	const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
 
 	return (
 		<div className={css.wrapper}>
-			<Button selected={filter === statusFilters.all}>All</Button>
-			<Button selected={filter === statusFilters.active}>Active</Button>
-			<Button selected={filter === statusFilters.completed}>Completed</Button>
+			<Button
+				selected={filter === statusFilters.all}
+				onClick={() => handleFilterChange(statusFilters.all)}
+			>
+				All
+			</Button>
+			<Button
+				selected={filter === statusFilters.active}
+				onClick={() => handleFilterChange(statusFilters.active)}
+			>
+				Active
+			</Button>
+			<Button
+				selected={filter === statusFilters.completed}
+				onClick={() => handleFilterChange(statusFilters.completed)}
+			>
+				Completed
+			</Button>
 		</div>
 	);
 };
