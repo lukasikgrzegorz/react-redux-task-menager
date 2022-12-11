19bf691374f1ada2fabc07f42c24bb9313e114dc
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-11 14:19:50 +0100

    readux toolkit slices

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -2,11 +2,13 @@ import { Button } from "../../components/Button/Button";
 import css from "./StatusFilter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { statusFilters } from "../../redux/constants";
-import { setStatusFilter } from "../../redux/actions";
+import { getStatusFilter } from "../../redux/selectors";
+import { setStatusFilter } from "../../redux/filtersSlice";
 
 export const StatusFilter = () => {
 	const dispatch = useDispatch();
-	const filter = useSelector((state) => state.filters.status);
+	const filter = useSelector(getStatusFilter);
+
 	const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
 
 	return (
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+import { configureStore } from "@reduxjs/toolkit";
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
+
+export const store = configureStore({
+	reducer: {
+		tasks: tasksReducer,
+		filters: filtersReducer,
+	},
+});
